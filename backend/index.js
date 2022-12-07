3b24d38f3e639aa1682faa180dd3253200a51251
John Doe <hamedhasan422421@gmail.com>
2022-12-07 13:14:43 +0300

    backend update with mongodb

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+const dbConnect = require("./utils/dbConnect");
+const { connectToServer } = require("./utils/dbConnect");
+const errorHandler = require("./middleware/errorHandler");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+connectToServer((err) => {
+    if(!err) {
+        app.listen(port, () => {
+            console.log(`app listening on port ${port}`);
+        })
+    }else {
+        console.log(err)
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.all("*", (req, res) => {
+    res.send("NO route found.");
+  });
+
+app.use(errorHandler);
+
+process.on("unhandledRejection", (error) => {
+    console.log(error.name, error.message);
+    app.close(() => {
+        process.exit(1);
+    })
+})
