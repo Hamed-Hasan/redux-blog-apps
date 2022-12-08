6a048736d09b06247df950e03b9591df6a0b894e
John Doe <hamedhasan422421@gmail.com>
2022-12-08 10:25:36 +0300

    find blog detail api

--- backend/controllers/blogsController.js
+++ backend/controllers/blogsController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../utils/dbConnect");
 
 module.exports.createTool = async (req, res, next) => {
@@ -20,7 +21,23 @@ module.exports.getAllBlogs = async (req, res, next) => {
  try {
     const db = getDb();
     const blogs = await db.collection("blogs").find({}).toArray();
-    res.status(200).send(blogs);
+    res.status(200).send({success: true, data: blogs});
+ } catch (error) {
+    next(error)
+ }
+}
+module.exports.getBlogDetail = async (req, res, next) => {
+ try {
+    const db = getDb();
+    const { id } = req.params;
+    if(!ObjectId.isValid(id)) {
+        return res.status(400).send({success:false, message: "Invalid ID"})
+    }
+    const blogs = await db.collection("blogs").findOne({_id: ObjectId(id)})
+    if(!blogs) {
+        return res.status(400).send({success: false, message: "Could'n find the blog please try again later!!!"})
+    }
+    res.status(200).send({success:true, data: blogs});
  } catch (error) {
     next(error)
  }
--- backend/routes/blogsRoutes.js
+++ backend/routes/blogsRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const blogsController = require('../controllers/blogsController');
 
-router.route('/')
+router.route("/")
 .post(blogsController.createTool)
 .get(blogsController.getAllBlogs)
+
+router.route("/:id")
+.get(blogsController.getBlogDetail)
 module.exports = router;
